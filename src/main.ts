29ead1fe9375a9a52e57f124b9a419063e205b52
Simaan <smaan.1997@gmail.com>
2022-04-25 20:24:36 +0300

    try to add pwa add to home screen custom button

--- src/main.ts
+++ src/main.ts
@@ -7,6 +7,14 @@ import { environment } from './environments/environment';
 if (environment.production) {
   enableProdMode();
 }
+var prompt;
+window.addEventListener('beforeinstallprompt', function (e) {
+  // Prevent the mini-infobar from appearing on mobile
+  e.preventDefault();
+  // Stash the event so it can be triggered later.
+  prompt = e;
+  console.log('beforeinstallprompt fired!');
+});
 
 platformBrowserDynamic().bootstrapModule(AppModule)
   .catch(err => console.log(err));
